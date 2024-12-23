e657bdc553f001b00b5b8561d0d093575ea74384
Ivan Filatov <nymezide@gmail.com>
2024-12-23 21:20:53 +0500

    update CI update xunit to v3

--- tests/Specification.IntegrationTests/ResetDatabaseAttribute.cs
+++ tests/Specification.IntegrationTests/ResetDatabaseAttribute.cs
@@ -5,17 +5,18 @@ using System.Reflection;
 using System.Text;
 using Xunit;
 using Xunit.Sdk;
+using Xunit.v3;
 
 namespace Specification.IntegrationTests
 {
     public class ResetDatabaseAttribute : BeforeAfterTestAttribute
     {
-        public override void After(MethodInfo methodUnderTest)
+        public override void After(MethodInfo methodUnderTest, IXunitTest test)
         {
             using var db = new OrdersDbContext();
             db.Database.EnsureDeleted();
 
-            base.After(methodUnderTest);
+            base.After(methodUnderTest, test);
         }
     }
 }
